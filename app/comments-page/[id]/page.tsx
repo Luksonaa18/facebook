d2095d75d9275353c354054c7b19fcbb8ad4ed9c
Luksonaa18 <lukazhozhadze53@gmail.com>
2025-05-22 19:56:44 +0400

    new

--- app/comments-page/[id]/page.tsx
+++ app/comments-page/[id]/page.tsx
@@ -1,11 +1,15 @@
-"use client";
+
 import React from "react";
 import Comment from "../../components/comment/Comment";
 type Props = {
   params: { id: string };
 };
 
-export default function Page({ params }: Props) {
-  const { id } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return <Comment id={id} />;
 }
